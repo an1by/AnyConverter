d9ed3c74a4b0a119f2daf826cb264d98e546ad06
An1by <anibycor@gmail.com>
2025-03-10 08:16:31 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using AnyConverter;
+
+namespace AnyController
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputFilePath = args[0];
+            string outputExtension = args[1];
+            string? scale = args.Length >= 3 ? args[2] : null;
+            Console.WriteLine(scale);
+
+            string fileName = Path.GetFileName(inputFilePath);
+            string[] split = fileName.Split('.');
+            int lastIndex = split.Length - 1;
+            string fileExtension = split[lastIndex];
+            split = split.Take(lastIndex).ToArray();
+            string fileNameWithoutExt = String.Join(".", split);
+
+            var videoConverter = new VideoConverter(inputFilePath, fileNameWithoutExt, scale);
+            if (videoConverter.TryConvert(fileExtension, outputExtension))
+            {
+                return;
+            }
+
+            Console.WriteLine("There is no variant to convert it!");
+        }
+    }
+}
---
+++ VideoConverter.cs
@@ -0,0 +1,71 @@
+﻿using System.Diagnostics;
+
+namespace AnyConverter
+{
+    internal class VideoConverter
+    {
+        private const string ffmpegPath = "ffmpeg.exe";
+        private readonly string inputFilePath;
+        private readonly string fileName;
+        private readonly string? scale;
+
+        public VideoConverter(string inputFilePath, string fileName, string? scale)
+        {
+            this.inputFilePath = inputFilePath;
+            this.fileName = fileName;
+            this.scale = scale;
+        }
+
+        public bool TryConvert(string inptuFileExtension, string outputFileExtension)
+        {
+            inptuFileExtension = inptuFileExtension.ToLower();
+            switch (outputFileExtension)
+            {
+                case "webm":
+                    switch (inptuFileExtension)
+                    {
+                        case "gif":
+                            GifToWebm();
+                            return true;
+                        case "png":
+                            PngToWebm();
+                            return true;
+                    }
+                    break;
+            }
+            return false;
+        }
+
+        public bool PngToWebm()
+        {
+            return GifToWebm();
+        }
+
+        public bool GifToWebm()
+        {
+            var outputFilePath = Path.Combine(inputFilePath, @"../", $"{fileName}.webm");
+
+            var ffmpegStringBuilder = new List<string>() { "-y -i \"{0}\"" };
+            if (scale != null)
+            {
+                ffmpegStringBuilder.Add($"-s {scale}");
+            }
+            ffmpegStringBuilder.Add("-b:v 0 -crf 25 \"{1}\"");
+
+            var ffmpegString = String.Join(" ", ffmpegStringBuilder.ToArray());
+
+            Console.WriteLine(string.Format(ffmpegString, inputFilePath, outputFilePath));
+
+            using (var process = Process.Start(ffmpegPath, string.Format(ffmpegString, inputFilePath, outputFilePath)))
+            {
+                process.WaitForExit();
+                if (process.ExitCode == 0)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
